c29802a2af5779f06bfa3b77fdf77d49cbb95f8d
Julian Montez <julian.montez@gmail.com>
2014-08-06 16:35:13 -0500

    Performed a refactor and documentation

--- sshkeys.py
+++ sshkeys.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 import argh
 import ConfigParser
-import os
 import json
 import socket
 
+from os.path import expanduser
 from subprocess import check_call, CalledProcessError
 
 
 def run_cmd(command):
-    """
-    @param cmd
-    @return A map based on pass / fail run info
+    """ Runs a command and returns an array of its results
+
+    :param command: String of a command to run within a shell
+    :returns: Dictionary with keys relating to the execution's success
     """
     try:
         ret = check_call(command, shell=True)
@@ -23,37 +24,48 @@ def run_cmd(command):
                 'command': command}
 
 
-def get_targets(ini_file):
+def get_hosts(inventory):
+    """ Returns valid IP addresses from an INI file
+
+    :param inventory: Path to an INI file
+    :returns: List of strings
+    """
 
     def valid_ip(address):
         try:
+            # converts a string to 32-bit binary to test validity of address
             socket.inet_aton(address)
             return True
         except socket.error:
             return False
 
+    # 'allow_no_value' will allow our INI file to not require a key-value pair
     config = ConfigParser.ConfigParser(allow_no_value=True)
-    config.readfp(open(ini_file))
-    _tmp = []
-    for section in config.sections():
-        options = config.options(section)
-        for option in options:
-            if valid_ip(option):
-                _tmp.append(option)
-    return set(_tmp)
+    config.read(expanduser(inventory))
+
+    # filter out invalid IP addresses in each section
+    return set(option for section in config.sections()
+               for option in config.options(section) if valid_ip(option))
 
 
-def main(inventory=None, credentials=None):
-    targets = get_targets(os.path.expanduser(inventory))
-    for target in targets:
-        with open(credentials, 'r') as fp:
-            ini_file = json.load(fp)
-            username = ini_file['username']
-            password = ini_file['password']
-            command = ('sshpass -p {} ssh-copy-id'
-                       ' -o UserKnownHostsFile=/dev/null'
-                       ' -o StrictHostKeyChecking=no {}@{}')
-            run_cmd(command.format(password, username, target))
+def main(inventory=None, configuration=None):
+    """ Given an inventory and configuration file, place SSH keys onto
+        designated hosts
 
+    :param inventory: Path to an INI file
+    :param configuration: Path to a configuration file with credentials
+    """
+    hosts = get_hosts(inventory)
+
+    with open(configuration, 'r') as fp:
+        credentials = json.load(fp)
+        username = credentials['username']
+        password = credentials['password']
+
+    for host in hosts:
+        command = ('sshpass -p {password} ssh-copy-id'
+                   ' -o UserKnownHostsFile=/dev/null'
+                   ' -o StrictHostKeyChecking=no {username}@{host}')
+        run_cmd(command.format(username=username, password=password, host=host))
 
 argh.dispatch_command(main)
